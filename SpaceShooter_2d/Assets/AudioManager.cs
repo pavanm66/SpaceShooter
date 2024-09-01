0bba8e01b37973acb58c596667c8c9f3b46236d6
pavanm66 <pavanm66@gmail.com>
2024-09-01 13:20:32 +0530

    deleted library

--- SpaceShooter_2d/Assets/AudioManager.cs
+++ SpaceShooter_2d/Assets/AudioManager.cs
@@ -5,5 +5,28 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
    private bool isSoundOn;
+   private bool isVibrationOn;
+   private bool isMusicOn;
+
+   public bool IsSoundOn{
+      get{
+         return value;
+      }
+      set{
+         isSoundOn = value;
+         PlayerPrefs.SetBool("isSoundOn",isSoundOn);
+      }
+   }
+   public bool IsVibrationOn{
+      get{
+         return value;
+
+      }
+      set{
+         isVibrationOn=value;
+         PlayerPrefs.SetBool("isVibrationOn",isVibrationOn);
+      }
+   }
+
    
 }
